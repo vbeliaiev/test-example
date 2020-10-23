a477d54c1705d53d243985718ba0447f80f38fe7
Vladyslav Beliaiev <vbeliaiev@makemusic.com>
2020-10-23 12:35:47 +0200

    Add serializers

---
+++ bundle_serializer.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+module Notation
+  class BundleSerializer
+    include JSONAPI::Serializer
+    include PlayUrlConcern
+
+    set_type 'bundles'
+    set_key_transform :dash
+
+    attributes :title,
+               :sm_classic_id,
+               :music_xml_file_type,
+               :score_type,
+               :updated_at
+
+    attribute :practice_play_url do |object|
+      play_bundle_url(object, object.bundles_instruments.first)
+    end
+
+    attribute :music_file_url do |object|
+      music_url(object)
+    end
+
+    attribute :music_xml_url do |object|
+      music_url(object)
+    end
+
+    has_many :bundles_instruments, lazy_load_data: true, links: {
+      related: ->(object) {
+        url_helpers.notation_bundle_bundles_instruments_url(bundle_id: object.id)
+      }
+    }
+
+    has_many :movements, serializer: Notation::MovementSerializer
+
+    def self.music_url(object)
+      if Rails.env.development? && !Dragonfly.app.datastore.is_a?(Dragonfly::S3DataStore)
+        object.music_xml.url
+      else
+        object.music_xml.remote_url(expires: 10.minutes.from_now)
+      end
+    end
+  end
+end
